d7602d1f33710f80b51c3cc8563dd8090f6409b2
kellyvigil <kellywvigil@gmail.com>
2014-02-16 01:15:15 -0800

    lots of changes

--- UONYC2014.js
+++ UONYC2014.js
@@ -18,7 +18,7 @@ function sayHello(){
 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
-
+/*
    $('#myAffix').affix({
     offset: {
       top: 10
@@ -27,3 +27,57 @@ function sayHello(){
       }
     }
   })
+*/
+
+
+
+
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= $('.open-image').height()) {
+      $('.afix-top').addClass('fix-to-top');
+    } else {
+      $('.afix-top').removeClass('fix-to-top');
+    }
+  });
+
+
+
+
+
+
+  var show_lightbox = function(contents) {
+    //console.log('show_lightbox');
+    window.light_box_active = true;
+    $('.lightbox-wrapper').css('display', 'block');
+    $('.lightbox').html(contents).css('display', 'block');    
+  }
+  var hide_lightbox = function() {
+    //console.log('hide_lightbox');
+    $('.lightbox-wrapper').css('display', 'none');
+    $('.lightbox').html('');  
+    window.light_box_active = false;
+  }
+
+  $('.bio-box').on('click', function(el) {      
+      var $inner = $(this).find('.inner');
+      //var $inner_img = $inner.find('img');
+      //console.log($inner_img);
+      //var $inner_img.attr('src', '');
+      var html = $inner.html();
+      //console.log(html);      
+      show_lightbox(html);
+      //var name = $inner.find('h2').text();
+      //console.log('name', name);
+      
+    });
+    $('.lightbox').on('click', function(e) {
+      e.stopPropagation();
+    })
+    $('.lightbox-wrapper').on('click', function(el) {
+      if (window.light_box_active === true) {
+        hide_lightbox();
+      }
+    });
+});   
