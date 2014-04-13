267bbf6e8a0aa12301b0a3a1ef43c0554f4feb7e
kellyvigil <kellywvigil@gmail.com>
2014-04-13 13:48:28 -0700

    top thing

--- UONYC2014.js
+++ UONYC2014.js
@@ -78,6 +78,18 @@ function sayHello(){
     }
   });
 
+  
+
+var target = $('.title');
+var targetHeight = target.outerHeight();
+
+$(document).scroll(function(e){
+    var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
+    if(scrollPercent >= 0){
+        target.css('opacity', scrollPercent);
+    }
+});
+
 
 
 
