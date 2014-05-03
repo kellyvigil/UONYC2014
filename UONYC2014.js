287fbac4c3a015cf37e85a06d6d2fbfbdcd1334a
kellyvigil <kellywvigil@gmail.com>
2014-05-03 17:42:30 -0400

    background

--- UONYC2014.js
+++ UONYC2014.js
@@ -23,6 +23,8 @@ function sayHello(){
 
 
 
+
+
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
 /*
@@ -147,11 +149,11 @@ $(document).ready(function (){
 
   
 
-var target = $('.title');
+var target = $('.title-cover');
 var targetHeight = target.outerHeight();
 
 $(document).scroll(function(e){
-    var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
+    var scrollPercent = (targetHeight - window.scrollY*2) / targetHeight;
     if(scrollPercent >= 0){
         target.css('opacity', scrollPercent);
       }
